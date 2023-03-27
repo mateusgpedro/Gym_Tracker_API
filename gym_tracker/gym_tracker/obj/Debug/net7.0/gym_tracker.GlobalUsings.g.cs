f033b79d6cd746fd184f94d7eae82bfac795e06f
Mateus Guimaraes Pedro <mateusgpedro.6@gmail.com>
2023-03-28 00:33:17 +0100

    Added Package

--- gym_tracker/gym_tracker/obj/Debug/net7.0/gym_tracker.GlobalUsings.g.cs
+++ gym_tracker/gym_tracker/obj/Debug/net7.0/gym_tracker.GlobalUsings.g.cs
@@ -2,6 +2,7 @@
 global using global::Microsoft.AspNetCore.Builder;
 global using global::Microsoft.AspNetCore.Hosting;
 global using global::Microsoft.AspNetCore.Http;
+global using global::Microsoft.AspNetCore.Mvc;
 global using global::Microsoft.AspNetCore.Routing;
 global using global::Microsoft.Extensions.Configuration;
 global using global::Microsoft.Extensions.DependencyInjection;
